4cdb460d187e5770b499f7ca559e9459becc95d4
Melisa Quesada <Mel@99.200-57-1.bestel.com.mx>
2019-01-29 11:44:07 -0600

    Agregue card.html, form.html, help.html y Actualice codigo en archivos de Javascript

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,42 @@
 window.cipher = {
-  // ... 
+  offset: 0,
+  string: '',
+
+  //método para obtener offset
+  const getOffset = () => {
+    offset = document.getElementsByName('offset').value;
+  }
+  
+  //método para guardar el valor escrito por el usuario y pasarlo a letras mayúsculas
+  const getInfo = (info) => {
+    string = (document.getElementsById(info).value).toUpperCase();
+  }
+
+//método para codificar
+  let encode: (offset, string) => {
+    let encodeString = '';
+    for(let i = 0 ; i <= string.length ; i++) {
+      if(string === ' '){ //Si hay un espacio, dejarlo igual
+        encodeString += ' ';
+      } else{
+        let x = (string.charCodeAt(i) - 65 + offset) % 26 + 65;
+        encodeString += (String.fromCharCode(x));
+      }
+    }
+    return encodeString;
+  }
+
+//método para decodificar
+  let decode: (offset, string) => {
+    let decodeString = '';
+    for(let j = 0 ; j <= string.length ; j++) {
+      if(string === ' '){
+        decodeString += ' ';
+      } else{
+        let y = (string.charCodeAt(i) - 65 - offset) % 26 + 65;
+        decodeString += (String.fromCharCode(y));
+      }
+    }
+    return decodeString;
+  }
 };
---
+++ src/index.js
@@ -0,0 +1,9 @@
+//Ir a la página del formulario
+const toForm() => {
+  window.location = 'form.html';
+}
+
+const toCard() => {
+  window.location = 'card.html';
+}
+
