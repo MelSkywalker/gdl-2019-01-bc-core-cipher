a972ae01c84ee3e3e561bb0297778c616344d899
Melisa Quesada <melisa.quc@gmail.com>
2019-01-30 11:22:11 -0600

    Ya pasó los test

--- src/cipher.js
+++ src/cipher.js
@@ -1,26 +1,29 @@
 window.cipher = {
-/*
+
   offset: 0,
   string: '',
 
+/*
   //método para obtener offset
-  const getOffset = () => {
-    offset = document.getElementsByName('offset').value; //'offset' u offset ???
+  getOffset = () => {
+    offset = parseInt(document.getElementsByName('offset')).value; //'offset' u offset ???
     return offset;
   }
   
   //método para guardar el valor escrito por el usuario y pasarlo a letras mayúsculas
-  const getString = (userInfo) => {
+  getString = (userInfo) => {
     string = (document.getElementsById('userInfo').value).toUpperCase(); // 'userInfo' o userInfo ???
     return string;
   }
-  */
+*/
 
 //método para codificar
-  let encode = (offset, string) => {
+  encode: function(offset, string) {
+
+    let array = string.split('');
     let encodedString = '';
     for(let i = 0 ; i < string.length ; i++) {
-      if(string === ' '){ //Si hay un espacio, dejarlo igual
+      if(array[i] === ' '){ //Si hay un espacio, dejarlo igual
         encodedString += ' ';
       } else{
         let x = (string.charCodeAt(i) - 65 + offset) % 26 + 65;
@@ -28,19 +31,20 @@ window.cipher = {
       }
     }
     return encodedString;
-  }
+  },
 
 //método para decodificar
-  let decode = (offset, string) => {
+  decode: function(offset, string) {
+    let array = string.split('');
     let decodedString = '';
     for(let i = 0 ; i < string.length ; i++) {
-      if(string === ' '){
+      if(array[i] === ' '){
         decodedString += ' ';
       } else{
-        let x = (string.charCodeAt(i) - 65 - offset) % 26 + 65;
+        let x = (string.charCodeAt(i) + 65 - offset) % 26 + 65;
         decodedString += (String.fromCharCode(x));
       }
     }
     return decodedString;
   }
-//};
+};
